6d6c1782486d80823fd26f08c81a6067fe3c453b
Joshua Oladeji <dejijolaoluwa@gmail.com>
2026-06-21 22:22:48 +0100

    feat: expose modules from lib.rs

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,5 @@ mod exercises;
 mod extension_fields;
 
 fn main() {
-    // let a = FieldElement::new(2, MODULUS).unwrap();
-    // let b = FieldElement::new(3, MODULUS).unwrap();
-
     extension_fields::compute::main();
-
-    // • (3, 6)
-    // let potential_g = Point::new(
-    //     a,
-    //     b,
-    //     Some(FieldElement::new(3, MODULUS).unwrap()),
-    //     Some(FieldElement::new(6, MODULUS).unwrap()),
-    // )
-    // .unwrap();
-
-    // for i in 1..=MODULUS {
-    //     let point: Point = potential_g.scalar_mul(i);
-    //     println!("{} => {}", i, &point);
-
-    //     if point.x.is_none() && point.y.is_none() {
-    //         break;
-    //     }
-    // }
 }
